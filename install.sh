035c557b8f92bac1b432dfe400f518b6589ef51c
Jim <jim@oddvalue.co.uk>
2021-01-30 13:13:17 +0000

    WIP: dot file setup

---
+++ install.sh
@@ -0,0 +1,13 @@
+# Install ZSH
+sudo apt install zsh
+chsh -s $(which zsh)
+
+# Install Prezto
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+# Install commands
+ln -s ./bin "${HOME}"
