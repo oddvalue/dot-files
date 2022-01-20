72fade9146bd7d2c2353a09bb4751e4dd039c839
jim <jim@bozboz.co.uk>
2022-01-20 14:24:50 +0000

    Add .p10k.zsh

--- symlinks.sh
+++ symlinks.sh
@@ -5,3 +5,6 @@ sudo chmod 770 $DOTFILES_DIR/bin/*
 # Install VIM presets
 ln -sf "$PWD/.vimrc" "$HOME/.vimrc"
 ln -sf "$PWD/.vim" "$HOME/.vim"
+
+# Powerline10K config
+ln -sf "$PWD/.p10k.zsh" "$HOME/.p10k.zsh"
