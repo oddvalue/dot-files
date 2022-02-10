e0cd768b2e12bbd4d66267146badede5b2bcd6ce
jim <jim@oddvalue.co.uk>
2022-02-10 13:43:58 +0000

    Fix battery alert cron

--- updates/battery-alert-cron.sh
+++ updates/battery-alert-cron.sh
@@ -1,1 +1,1 @@
-echo "@reboot $(whoami) battery-alert" | sudo tee /etc/cron.d/battery-alert
+echo "*/5 * * * * $(whoami) sh ${DOTFILES_DIR}/bin/battery-alert  > /dev/null 2>&1\n" | sudo tee /etc/cron.d/battery-alert
