861858d504a1b50413701179721fe3cff408dc47
jim <jim@oddvalue.co.uk>
2022-07-29 07:34:56 +0100

    Add some aliases for pint and docker

--- .aliases.zsh
+++ .aliases.zsh
@@ -53,7 +53,7 @@ fi
 alias timelog='git log --no-decorate --author=$GIT_NAME --oneline --format="- %s" --since=midnight'
 alias yesterdaylog='git log --no-decorate --author=$GIT_NAME --oneline --format="- %s" --since=yesterday.midnight --until=midnight'
 
-alias ds="rsync -avzh --exclude '.git' --exclude 'storage' --exclude '.env' --exclude '.idea' --exclude '.docker' --exclude 'bootstrap/cache/livewire-components.php' ~/Projects/tmstores ec2-user@dev:/home/"
+alias ds="rsync -avzh --exclude '.git' --exclude 'storage' --exclude '.env' --exclude '.idea' --exclude 'node_modules' --exclude '.docker' --exclude 'bootstrap/cache/livewire-components.php' ~/Projects/tmstores ec2-user@dev:/home/"
 alias dsd="ds --delete --ignore-errors"
 
 alias dockerssh="docker exec -it tmstores_web /bin/bash"
@@ -63,3 +63,7 @@ alias mcomposer="COMPOSER_MEMORY_LIMIT=-1 composer"
 alias tm="cd ~/Projects/tmstores"
 
 alias nordd="nordlayer disconnect"
+
+alias da="docker exec -it -u apache tmstores_web php artisan"
+
+alias pint="./vendor/bin/pint"
